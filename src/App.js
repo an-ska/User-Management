d79310ab93f42b67fb1e2da7208d276b533b2d60
an-ska <1.anna.piotrowska@gmail.com>
2018-07-30 21:34:40 +0200

    Initial commit with setup of create-react-apps

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserManagementApp from "./containers/UserManagementApp";
+
+class App extends Component {
+  render() {
+    return (
+      <UserManagementApp />
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/UserManagementApp/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styles from "./UserManagementApp.css";
+
+class UserManagementApp extends Component {
+  render() {
+    return (
+      <p>TEST</p>
+    )
+  }
+}
+
+export default UserManagementApp;
